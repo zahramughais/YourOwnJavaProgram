70c0efd643341500e645875c4cad285ac1c7288b
Zahra Mughais <zahramughais@gmail.com>
2022-07-17 13:35:23 +0300

    first java

---
+++ Zahra.java
@@ -0,0 +1,7 @@
+public class Zahra{
+    public static void main(String[] args){
+        System.out.println("My name is Zahra");
+        System.out.println("I am 100 years old");
+        System.out.println("My hometown is Qatif, EP");
+    }
+}
